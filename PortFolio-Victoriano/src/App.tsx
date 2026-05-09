c0f2882d2d8a96ceef396e59f6b19473bc116a2c
VictorianoDL <Vitivitoriano@gmail.com>
2026-05-09 15:07:57 -0300

    corregidos errores de render

--- PortFolio-Victoriano/src/App.tsx
+++ PortFolio-Victoriano/src/App.tsx
@@ -1,7 +1,5 @@
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
 import Main from './Components/2-Main/Main'
-import Footer from './Components/3-Footer/Footer'
 
 function App() {
     
--- PortFolio-Victoriano/src/Components/3-Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-      <footer>
-          <p> &copy: footer de pagina con react </p>
-      </footer>
-    );
-};
-
-export default Footer;
